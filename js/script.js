dc6d88bda7a3e56af99d2f6a1acab39741a9d369
ivanilsonjunior33 <ivanilsonararaujojr@gmail.com>
2020-05-15 16:38:39 -0300

    Adicionado contagem de moedas

--- js/script.js
+++ js/script.js
@@ -3,25 +3,42 @@ const caixaValor = document.querySelector('.contarnotas__valor');
 const botaoValor = document.querySelector('.contarnotas__botao');
 const divResultado = document.querySelector('.contarnotas__resultado');
 
-// Chamar função para dividir cedulas quando clickar no botão
-botaoValor.addEventListener("click", function(){
-    dividirCedulas(caixaValor.value)
+botaoValor.addEventListener("click", function(){ //Chamar função quando clickar no botão
+    dividirCedulas(Number(caixaValor.value))
 })
 
-// função para dividir as cedulas
-function dividirCedulas(notasInseridas){
-    const cedulas = [100,50,20,10,5,2,1]
-    let divisao = 0
-    let resultado = []
-    if(Math.floor(notasInseridas) > 0) {
-        for(i in cedulas){
-        divisao = Math.floor(notasInseridas / cedulas[i])
-        if(divisao > 0){
-            notasInseridas %= cedulas[i]
-            let resultadoDivisao = (`${divisao} nota(s) de ${cedulas[i]} R$`)
-            resultado.push(resultadoDivisao)
-            divResultado.innerHTML = resultado.join("<br><br>")
+
+function dividirCedulas(notasInseridas){ //Função calcular divisão
+    const cedulas = [100,50,20,10,5,2]
+    const moedas = [1,0.50,0.25,0.10,0.05]
+    let divisoes = 0
+    let totalNotas = []
+    let totalMoedas = []
+    if(notasInseridas == '' || notasInseridas < 0){
+        divResultado.innerHTML = (`<strong style="color:red;font-size:25px">ERRO: Digite um numero válido</strong>`)
+    }else{
+        for(c in cedulas){ //Divisao Cedulas
+        if(notasInseridas != 0){
+        divisoes = Math.floor(notasInseridas / cedulas[c])
+        if(divisoes > 0){
+            Math.floor(notasInseridas %= cedulas[c])
+            let qtdCedulas = (`${divisoes} nota(s) de R$ ${cedulas[c]}`)
+            totalNotas.push(qtdCedulas)
         }
     }
-}   else divResultado.innerHTML = (`<strong style="color:red;font-size:25px">ERRO: Digite um numero maior que 0</strong>`)
+}
+        for(let m in moedas){ //Divisao Moedas
+        if(notasInseridas != 0){
+        divisoes = Math.floor(notasInseridas / moedas[m])
+        if(divisoes > 0){
+            notasInseridas %= moedas[m]
+            notasInseridas = notasInseridas.toFixed(2)
+            let qtdMoedas = (`${divisoes} moedas(s) de R$ ${moedas[m]}`)
+            totalMoedas.push(qtdMoedas)
+        }
+    }
+}
+    let resultado = `${totalNotas.join('<br>')}<br>${totalMoedas.join('<br>')}`
+     divResultado.innerHTML = resultado
+}
 }
